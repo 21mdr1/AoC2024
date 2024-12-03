2b1cd9528a1d1161b38a7d0b93f0044176d1fbab
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-03 06:50:16 -0500

    solve day 1 part 1

--- day1/part1.py
+++ day1/part1.py
@@ -1,1 +1,7 @@
-input = open("../input/test/test-input1.txt", "r").read()
+input = open("../input/input1.txt", "r").read() # string
+
+list1 = sorted([int(x.split("   ")[0]) for x in input.split("\n")])
+list2 = sorted([int(x.split("   ")[1]) for x in input.split("\n")])
+
+difference = sum([ abs(list1[i] - list2[i]) for i in range(len(list1)) ])
+print(difference)
