5a854aea088fe658e7fa4b820c93d193154ffea4
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-05 14:28:05 -0500

    Add script for creating daily files

---
+++ helpers/create_files.sh
@@ -0,0 +1,9 @@
+# /bin/bash
+
+cd ~/OneDrive/Documents/Projects/AoC2024
+
+DAY=$1
+
+mkdir day${DAY}
+touch day${DAY}/part1.py day${DAY}/part2.py day${DAY}/README.md
+touch input/input${DAY}.txt input/test/test-input${DAY}.txt
