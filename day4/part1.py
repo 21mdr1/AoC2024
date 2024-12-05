8a369d3884e9e7d5eadb27b613e922207b7a422d
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-05 16:32:47 -0500

    Solve day 4 part 1

--- day4/part1.py
+++ day4/part1.py
@@ -1,24 +1,56 @@
-from collections.abc import Callable
+from utils import print_result, test, input_file
+from re import findall
 
-def input_file(day: int, test: bool = False) -> str:
-    test_text = "test/test-" if test else ""
-    return f"../input/{test_text}input{day}.txt"
-
-def print_result(func: Callable) -> Callable: 
-
-    def fn(*args, **kwargs):
-        answer = func(*args, **kwargs)
-        print(answer)
-        return answer
-
-    return fn
-
-@print_result
-def test(routine: Callable[[str], int], day: int, answer: int) -> bool:
-     return routine(input_file(day, True)) == answer
+XMAS = "XMAS"
+SAMX = "SAMX"
 
 @print_result
 def solve(input_file: str) -> int:
-    return 0
+    search = open(input_file, "r").read().split("\n")
+
+    # horizontal
+    total_h = sum([ check(line) for line in search ])
+
+    # vertical   
+    total_v = sum([ check(line) for line in transpose(search) ])
+
+    # diagonal
+    total_d = sum([ check(line) for line in diagonals(search) ])
+
+    return total_h + total_v + total_d
+
+def check(line: str) -> int:
+    return len(findall(XMAS, line)) + len(findall(SAMX, line))
+
+def transpose(matrix: list[str]) -> list[str]:
+    return [ ''.join([ line[i] for line in matrix ]) for i in range(len(matrix[0])) ]
+
+def diagonals(matrix: list[str]) -> list[str]:
+
+    right_leaning = []
+
+    for total in range(len(matrix[0]) * 2):
+        curr = ""
+        for i in range(len(matrix[0])):
+            for j in range(len(matrix[0])):
+                if i + j == total:
+                    curr += matrix[i][j]
+        
+        right_leaning.append(curr)
+
+    left_leaning = []
+
+    for total in range(-len(matrix[0]) + 1, len(matrix[0])):
+        curr = ""
+
+        for i in range(len(matrix[0])):
+            for j in range(len(matrix[0])):
+                if i - j == total:
+                    curr += matrix[i][j]
+        
+        left_leaning.append(curr)
+
+    return right_leaning + left_leaning
 
 test(solve, 4, 18)
+solve(input_file(4))
