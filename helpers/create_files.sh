88ce3565d678616414605494fdd34a924581af62
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-06 12:52:50 -0500

    Create file boilerplate templates

--- helpers/create_files.sh
+++ helpers/create_files.sh
@@ -5,9 +5,12 @@ cd ~/OneDrive/Documents/Projects/AoC2024
 DAY=$1
 
 mkdir day${DAY}
-touch day${DAY}/part1.py day${DAY}/part2.py
+
 touch input/input${DAY}.txt input/test/test-input${DAY}.txt
-cp helpers/utils.txt day${DAY}/utils.py
+
+cp helpers/templates/script.txt day${DAY}/part1.py
+cp helpers/templates/script.txt day${DAY}/part2.py
+cp helpers/templates/utils.txt day${DAY}/utils.py
 
 git add .
 git commit -m "Create files for day ${DAY}"
