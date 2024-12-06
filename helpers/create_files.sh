c2b942e889146a00ef68eba934422db584cb1665
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-06 10:46:58 -0500

    Add utils file to the create files script

--- helpers/create_files.sh
+++ helpers/create_files.sh
@@ -5,8 +5,9 @@ cd ~/OneDrive/Documents/Projects/AoC2024
 DAY=$1
 
 mkdir day${DAY}
-touch day${DAY}/part1.py day${DAY}/part2.py day${DAY}/README.md
+touch day${DAY}/part1.py day${DAY}/part2.py
 touch input/input${DAY}.txt input/test/test-input${DAY}.txt
+cp helpers/utils.txt day${DAY}/utils.py
 
 git add .
 git commit -m "Create files for day ${DAY}"
