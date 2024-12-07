0ef9f3eef699f653066ebe12eb1723db68486bed
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-07 16:03:19 -0500

    Adjust non concat code for part 2

--- day7/part2.py
+++ day7/part2.py
@@ -6,33 +6,37 @@ def parse_equation(equation: str):
 
     return { "result": int(result), "nums": nums }
 
+def test_without_concat(equation) -> bool:
+    total_configs = 2 ** (len(equation["nums"]) - 1)
+    length = len(bin(total_configs)[2:]) - 1
+
+    for configuration in range(total_configs):
+        configuration = bin(configuration)[2:].rjust(length, "0")
+
+        total = equation["nums"][0]
+
+        for i, num in enumerate(equation["nums"][1:]):
+            if configuration[i] == "0":
+                total += num
+            elif configuration[i] == "1":
+                total *= num
+        
+        if total == equation["result"]:
+            return True
+
 @print_result
 def solve(input_file: str) -> int:
-    input = open(input_file, "r").read().split("\n")
-
-    equations = [ parse_equation(equation) for equation in input ]
+    equations = [ parse_equation(equation) 
+                 for equation in open(input_file, "r").read().split("\n") ]
 
     calibration_value = 0
 
     for equation in equations:
-        total_configs = 2 ** (len(equation["nums"]) - 1)
+        if test_without_concat(equation):
+            calibration_value += equation["result"]
+            continue
 
-        length = len(bin(total_configs)[2:]) - 1
-
-        for configuration in range(total_configs):
-            configuration = bin(configuration)[2:].rjust(length, "0")
-
-            total = equation["nums"][0]
-
-            for i, num in enumerate(equation["nums"][1:]):
-                if configuration[i] == "0":
-                    total += num
-                elif configuration[i] == "1":
-                    total *= num
-            
-            if total == equation["result"]:
-                calibration_value += equation["result"]
-                break
+        # code for concatenations
 
     return calibration_value
 
