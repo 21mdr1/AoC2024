811938f38c985e2998edd6aeaef9fa5a71c7f425
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-09 19:38:11 -0500

    Add code to parse input map

--- day8/part1.py
+++ day8/part1.py
@@ -1,10 +1,22 @@
 from utils import print_result, input_file, test
 
+
+def parse_map(antenna_map):
+    antennas = dict()
+
+    for i, j in [(i, j) for i in range(len(antenna_map)) for j in range(len(antenna_map))]:
+        if antenna_map[i][j] != '.':
+            antennas.get(antenna_map[i][j]).append((i, j))
+    
+    return antennas
+
 @print_result
 def solve(input_file: str) -> int:
-    input = open(input_file, "r").read()
+    input = open(input_file, "r").read().split("\n")
+    antennas = parse_map(input)
+
     return 0
 
 
-test(solve, _, _)
-solve(input_file(_))
+test(solve, 8, 14)
+solve(input_file(8))
