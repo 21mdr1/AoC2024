e912be6d31884ff424b16548497a4c5bdc733720
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-11 16:38:07 -0500

    Parse input

--- day10/part1.py
+++ day10/part1.py
@@ -1,11 +1,20 @@
-from utils import print_result, input_file, test
+from utils import print_result, input_file, test, use_input_file
 
 @print_result
 @use_input_file
-def solve(input_file: str) -> int:
-    input = open(input_file, "r").read()
+def solve(input: str) -> int:
+    topomap = [ list(map(int, line.split(""))) for line in input.split("\n") ]
+
+    trail_heads = []
+
+    for i, j in [ (x, y) for x in range(len(topomap)) for y in range(len(topomap[0])) ]:
+        if topomap[i][j] == 0:
+            trail_heads.append((i, j))
+
+    # evaluate each trailhead
+
     return 0
 
 
-test(solve, input_file(_, True), _)
-solve(input_file(_))
+test(solve, input_file(10, True), 36)
+# solve(input_file(10))
