6f0bbbf164d1d0bbb461361086b7fb897847fbbb
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-13 16:22:43 -0500

    Solve day 11 part 2

--- day11/part2.py
+++ day11/part2.py
@@ -3,23 +3,23 @@ from data import stoneCycles
 
 def calc(startStone, blinks):
     if blinks == 0:
-        return [ startStone ]
+        return 1
     
-    if (blinks in stoneCycles) and (startStone in stoneCycles[blinks]):
+    if blinks not in stoneCycles:
+        stoneCycles[blinks] = dict()
+
+    if startStone in stoneCycles[blinks]:
         return stoneCycles[blinks][startStone]
 
     if startStone == '0':
-        ans = [ *calc('1', blinks - 1) ]
+        ans = calc('1', blinks - 1)
     elif len(startStone) % 2 == 0:
-        ans = [ 
-            *calc(str(int(startStone[:len(startStone)//2])), blinks - 1), 
-            *calc(str(int(startStone[(len(startStone)//2):])), blinks - 1) 
-        ]
+        ans = calc(str(int(startStone[:len(startStone)//2])), blinks - 1) +\
+            calc(str(int(startStone[(len(startStone)//2):])), blinks - 1)
     else:
-        ans = [ *calc(str(int(startStone) * 2024), blinks - 1) ]
+        ans = calc(str(int(startStone) * 2024), blinks - 1)
 
-    if blinks in stoneCycles:
-        stoneCycles[blinks][startStone] = tuple(ans)
+    stoneCycles[blinks][startStone] = ans
 
     return ans
 
@@ -27,16 +27,8 @@ def calc(startStone, blinks):
 @use_input_file
 def solve(input: str) -> int:
     stones = input.strip('\n').split(' ')
-    cycles = 5
+    return sum([ calc(stone, 75) for stone in stones ])
 
-    for _ in range(cycles):
-        new_stones = []
-        for stone in stones:
-            new_stones.extend(calc(stone, 5))
-        stones = new_stones
-    
-    return len(stones)
-
-test(solve, input_file(11, True), 55312)
+# test(solve, input_file(11, True), 55312)
 # test(solve, input_file(11, False), 207683)
-#solve(input_file(11))
+solve(input_file(11))
