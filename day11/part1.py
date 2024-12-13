321ea851c331826ca5a732ef98dfca6819002c7e
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-13 12:03:27 -0500

    Solve day 11 part 1

--- day11/part1.py
+++ day11/part1.py
@@ -3,8 +3,22 @@ from utils import print_result, input_file, test, use_input_file
 @print_result
 @use_input_file
 def solve(input: str) -> int:
-    return 0
+    stones = input.strip('\n').split(' ')
+
+    for _ in range(25):
+        new_stones = []
+        for stone in stones:
+            if stone == '0':
+                new_stones.append('1')
+            elif len(stone) % 2 == 0:
+                new_stones.append(str(int(stone[:len(stone)//2])))
+                new_stones.append(str(int(stone[(len(stone)//2):])))
+            else:
+                new_stones.append(str(int(stone) * 2024))
+        stones = new_stones
+            
+    return len(stones)
 
 
-test(solve, input_file(_, True), _)
-#solve(input_file(_))
+# test(solve, input_file(11, True), 55312)
+solve(input_file(11))
