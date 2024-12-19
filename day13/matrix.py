590cb0d1094e05b95f2cd20b7cea0e060ec06dce
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-19 17:10:46 -0500

    Write logic for day 13 part 1

---
+++ day13/matrix.py
@@ -0,0 +1,119 @@
+class Matrix:
+    # this will only handle 2x2 and 1x2 matrices, which is all we need
+    def __init__(self, matrix):
+        if len(matrix) != 2:
+            raise ValueError
+        
+        self.isSquare = len(matrix[0]) == 2
+
+        if self.isSquare:
+            self.a, self.b = matrix[0]
+            self.c, self.d = matrix[1]
+        else:
+            self.a = matrix[0][0]
+            self.b = matrix[1][0]
+
+    def determinant(self):
+        if not self.isSquare:
+            return None
+        return self.a*self.d - self.b*self.c
+    
+    def isInvertible(self):
+        det = self.determinant()
+        return det is not None and det != 0
+    
+    def inverse(self):
+        if not self.isInvertible():
+            return None
+
+        det = self.determinant()
+
+        matrix = [
+            [ self.d, -1 * self.b ],
+            [ -1 * self.c, self.a ]
+        ]
+
+        return Matrix(matrix) / det
+    
+    def __mul__(self, other):
+        if isinstance(other, int):
+            if self.isSquare:
+                return Matrix([[self.a * other, self.b * other], [self.c * other, self.d * other]])
+            return Matrix([[self.a * other], [self.b * other]])
+
+        if isinstance(other, Matrix):
+            if not self.isSquare:
+                raise ValueError
+            
+            return Matrix([
+                [ self.a * other.a + self.b * other.b ],
+                [ self.c * other.a + self.d * other.b ]
+            ])
+        
+        else:
+            raise TypeError
+    
+    def __rmul__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            if self.isSquare:
+                return Matrix([[self.a * other, self.b * other], [self.c * other, self.d * other]])
+            return Matrix([[self.a * other], [self.b * other]])
+
+        if isinstance(other, Matrix):
+            if not other.isSquare or self.isSquare:
+                raise ValueError
+            
+            return Matrix([
+                [ other.a * self.a + other.b * self.b ],
+                [ other.c * self.a + other.d * self.b ]
+            ])
+        
+        else:
+            raise TypeError
+
+    def __truediv__(self, other):
+        # maybe I'll implement mult by inverse as matrix div but idk if that's worth it
+        if not isinstance(other, int) and not isinstance(other, float):
+            raise TypeError
+        
+        return Matrix([
+            [self.a / other, self.b / other],
+            [self.c / other, self.d / other]
+        ]) if self.isSquare else Matrix([
+            [self.a / other],
+            [self.b / other]
+        ])
+    
+    def __floordiv__(self, other):
+        if not isinstance(other, int) and not isinstance(other, float):
+            raise TypeError
+        
+        return Matrix([
+            [self.a // other, self.b // other],
+            [self.c // other, self.d // other]
+        ]) if self.isSquare else Matrix([
+            [self.a // other],
+            [self.b // other]
+        ])
+
+    def __repr__(self):
+        if self.isSquare:
+            return f'Matrix([[{self.a}, {self.b}], [{self.c}, {self.d}]])'
+        return f'Matrix([[{self.a}], [{self.b}]])'
+
+    def __str__(self):
+        if self.isSquare:
+            return f'{self.a} {self.b}\n{self.c} {self.d}'
+        return f'{self.a}\n{self.b}'
+    
+def determinant(matrix):
+    if not isinstance(matrix, Matrix):
+        return None
+    
+    return matrix.determinant()
+
+def inverse(matrix):
+    if not isinstance(matrix, Matrix):
+        return None
+    
+    return matrix.inverse()
--- day13/part1.py
+++ day13/part1.py
@@ -1,10 +1,37 @@
 from utils import print_result, input_file, test, use_input_file
+from matrix import Matrix, inverse
+import re
+
+def parse(machine):
+    for i, line in enumerate(machine.split('\n')):
+            if i == 0:
+                Max, May = map(int, re.fullmatch(r'Button A: X\+(\d+), Y\+(\d+)', line).group(1,2))
+            elif i == 1:
+                Mbx, Mby = map(int, re.fullmatch(r'Button B: X\+(\d+), Y\+(\d+)', line).group(1,2))
+            else:
+                Px, Py = map(int, re.fullmatch(r'Prize: X=(\d+), Y=(\d+)', line).group(1,2))
+    return Matrix([[Max, Mbx], [May, Mby]]), Matrix([[Px], [Py]])
 
 @print_result
 @use_input_file
 def solve(input: str) -> int:
-    return 0
+    machines = input.split('\n\n')
 
+    tokens = 0
 
-test(solve, input_file(_, True), _)
-#solve(input_file(_))
+    for machine in machines:
+        coefs, prize = parse(machine)
+
+        coefs_inv = inverse(coefs) * coefs.determinant()
+        solution = (coefs_inv * prize) / coefs.determinant()
+
+        if int(solution.a) == solution.a and int(solution.b) == solution.b:
+            tokens += int(solution.a) * 3
+            tokens += int(solution.b)
+
+    return tokens
+
+test(solve, input_file(13, True), 480)
+solve(input_file(13))
+
+# 24911 is too low
