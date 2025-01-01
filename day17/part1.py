0ce5427425bbcd52705506a3540b53bf5d1a3c7e
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-12-31 22:27:06 -0500

    Rewrite day 17 part 1 as a class

--- day17/part1.py
+++ day17/part1.py
@@ -1,116 +1,76 @@
 from utils import print_result, input_file, test, use_input_file
+from re import search;
 
-OUTPUT = []
-A = None; B = None; C = None
-POINTER = 0
+class Computer:
+    def __init__(self, input):
+        self.output = []
+        self.pointer = 0
+        self.a, self.b, self.c, self.program = self.parse_input(input)
 
-def getA(): return A
-def getB(): return B
-def getC(): return C
+    def parse_input(self, input):
+        a, b, c, program = search(r'Register A: (\d+)\nRegister B: (\d+)\nRegister C: (\d+)\n\nProgram: (.*)', input).groups()
 
-combo = {
-    0: lambda: 0,
-    1: lambda: 1,
-    2: lambda: 2,
-    3: lambda: 3,
-    4: getA,
-    5: getB,
-    6: getC,
-}
+        return *map(int, (a, b, c)), list(map(int, program.split(',')))
 
-def adv(operand):
-    global A
-    A = getA() // (2**combo[operand]())
+    def combo(self, operand):
+        return [0, 1, 2, 3, self.a, self.b, self.c][operand]
 
-def bxl(operand):
-    global B
-    B = getB() ^ operand
+    def adv(self, operand):
+        self.a = self.a // (2**self.combo(operand))
 
-def bst(operand):
-    global B
-    B = combo[operand]() % 8
+    def bxl(self, operand):
+        self.b = self.b ^ operand
 
-def jnz(operand):
-    global POINTER
-    if getA() == 0:
-        POINTER += 2
-    else: POINTER = operand
+    def bst(self, operand):
+        self.b = self.combo(operand) % 8
 
-def bxc(_):
-    global B
-    B = getB() ^ getC()
+    def jnz(self, operand):
+        if self.a != 0: self.pointer = operand
 
-def out(operand):
-    global OUTPUT
-    OUTPUT.append(combo[operand]() % 8)
+    def bxc(self, _):
+        self.b = self.b ^ self.c
 
-def bdv(operand):
-    global B
-    B = getA() // (2**combo[operand]())
+    def out(self, operand):
+       self.output.append(self.combo(operand) % 8)
 
-def cdv(operand):
-    global C
-    C = getA() // (2**combo[operand]())
+    def bdv(self, operand):
+        self.b = self.a // (2**self.combo(operand))
 
-opcode = {
-    0: adv,
-    1: bxl,
-    2: bst,
-    3: jnz,
-    4: bxc,
-    5: out,
-    6: bdv,
-    7: cdv
-}
+    def cdv(self, operand):
+        self.c = self.a // (2**self.combo(operand))
+    
+    def opcode(self, code):
+        opcode = {
+            0: self.adv,
+            1: self.bxl,
+            2: self.bst,
+            3: self.jnz,
+            4: self.bxc,
+            5: self.out,
+            6: self.bdv,
+            7: self.cdv
+        }
 
-def output():
-    return ','.join(map(str, OUTPUT))
+        return opcode[code]
 
-def parse_input(input):
-    global A, B, C
+    def debug(self):
+        while self.pointer < len(self.program):
+            instruction = self.program[self.pointer]
+            operand = self.program[self.pointer + 1]
 
-    for i, line in enumerate(input.split('\n')):
-        if i == 0:
-            A = int(line.split(' ')[-1])
-        elif i == 1:
-            B = int(line.split(' ')[-1])
-        elif i == 2:
-            C = int(line.split(' ')[-1])
-        elif i == 4:
-            program = list(map(int, line.split(' ')[1].split(',')))
+            self.pointer += 2
 
-    return program
+            self.opcode(instruction)(operand)
 
+        return ','.join(map(str, self.output))
 
 @print_result
 @use_input_file
 def solve(input: str) -> str:
-    global POINTER, OUTPUT
+    computer = Computer(input)
+    return computer.debug()
 
-    program = parse_input(input)
-
-    while POINTER < len(program):
-        instruction = program[POINTER]
-        operand = program[POINTER + 1]
-
-        # print("instruction", instruction, "operand", operand)
-
-        opcode[instruction](operand)
-
-        # print("A", A, "B", B, "C", C)
-
-        if instruction == 3:
-            continue
-
-        POINTER += 2
-
-    res = output()
-
-    OUTPUT = []; POINTER = 0
-    return res
-
-
-# test(solve, input_file(17, True), "4,6,3,5,6,3,5,2,1,0")
-# test(solve, 'Register A: 10\nRegister B: 0\nRegister C: 9\n\nProgram: 5,0,5,1,5,4', '0,1,2')
-# test(solve, 'Register A: 2024\nRegister B: 0\nRegister C: 9\n\nProgram: 0,1,5,4,3,0', '4,2,5,6,7,7,7,7,3,1,0')
+test(solve, input_file(17, True), "4,6,3,5,6,3,5,2,1,0")
+test(solve, 'Register A: 10\nRegister B: 0\nRegister C: 9\n\nProgram: 5,0,5,1,5,4', '0,1,2')
+test(solve, 'Register A: 2024\nRegister B: 0\nRegister C: 9\n\nProgram: 0,1,5,4,3,0', '4,2,5,6,7,7,7,7,3,1,0')
 solve(input_file(17))
